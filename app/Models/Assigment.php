6593cee222e94eaa38a1d7dede92addcefb98a18
Mikeu Dev <rikiruswandi28@gmail.com>
2025-12-06 11:58:11 +0700

    feat(model):assigment

---
+++ app/Models/Assigment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assigment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'course_id',
+        'title',
+        'description',
+        'due_date',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
